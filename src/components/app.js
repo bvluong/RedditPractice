f29611912f3fe44f09410dde342557ad102754e7
bvluong <bryantvinluong@gmail.com>
2017-07-13 20:28:24 -0700

    Add store, update reducer

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import TabContainer from './tabs/tab_container';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <TabContainer/>
+      </div>
+    );
+  }
+}
