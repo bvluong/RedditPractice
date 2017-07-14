c9b8f2f90876e3f6db0ed71aa7240fb08659a0eb
bvluong <bryantvinluong@gmail.com>
2017-07-13 20:35:53 -0700

    Add text input box

--- src/components/search_bar/search_bar.jsx
+++ src/components/search_bar/search_bar.jsx
@@ -4,12 +4,21 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = { input: ""};
+    this.updateHandler = this.updateHandler.bind(this);
+  }
+
+  updateHandler(e) {
+    e.preventDefault();
+    this.setState({input: e.target.value});
   }
 
   render() {
     return(
       <div>
         <form>
+          <input type="text"
+            value={this.state.input}
+            onChange={this.updateHandler}/>
           <input type="submit"/>
         </form>
       </div>
